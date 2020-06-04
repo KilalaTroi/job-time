ed35827b0c0b27a897ddf6bc22bdadca978e90d5
KILALA_Troi <troi.hoang@kilala.vn>
2020-06-04 16:39:10 +0700

    check editor

--- resources/assets/js/admin/pages/Ckeditor/Index.vue
+++ resources/assets/js/admin/pages/Ckeditor/Index.vue
@@ -7,7 +7,41 @@
 </template>
 <script>
     import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
+    // import Table from '@ckeditor/ckeditor5-table/src/table';
+    // import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+    // import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
+    // import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperties';
     // import '@ckeditor/ckeditor5-build-decoupled-document/build/translations/ja';
+
+    class UploadAdapter {
+        constructor(loader) {
+            this.loader = loader;
+        }
+
+        upload() {
+            return new Promise((resolve, reject) => {
+                const data = new FormData();
+                data.append('file', this.loader.file);
+                data.append('allowSize', 10);// M
+                axios.post('http://my-domains/path/to/uploadfile', data)
+                .then(res=>{
+                    if (res.file) {
+                        resolve({
+                            default: 'http://my-domains/uploads/' + data.file
+                        });
+                    } else {
+                        reject(res.msg);
+                    }
+                }).catch(err => {
+                    console.log(err);
+                });
+
+            });
+        }
+
+        abort() {
+        }
+    }
 
     export default {
         components: {
@@ -20,12 +54,13 @@
                 editorConfig: {
                     // The configuration of the editor.
                     // language: 'ja'
-                }
+                },
             };
         },
         methods: {
             onReady( editor )  {
                 // Insert the toolbar before the editable area.
+                console.log(editor.ui.view.toolbar.element);
                 editor.ui.getEditableElement().parentElement.insertBefore(
                     editor.ui.view.toolbar.element,
                     editor.ui.getEditableElement()
