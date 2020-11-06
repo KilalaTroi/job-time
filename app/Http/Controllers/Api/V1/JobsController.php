c40319c464ec6e9a1c16217d01072dc90967884b
troi.hoang <troi.hoang@kilala.vn>
2020-11-06 17:13:31 +0700

    add job default

--- app/Http/Controllers/Api/V1/JobsController.php
+++ app/Http/Controllers/Api/V1/JobsController.php
@@ -22,6 +22,7 @@ class JobsController extends Controller
         $userID = $_GET['user_id'];
         $showBy = $_GET['show'];
         $teamID = $_GET['team_id'];
+        $defaultProjects = array(58, 59, 68);
 
         // DB::enableQueryLog();
         if ( $showBy == 'showSchedule' ) {
@@ -38,13 +39,18 @@ class JobsController extends Controller
                 ->leftJoin('projects as p', 'p.id', '=', 'i.project_id')
                 ->join('schedules as s', 'i.id', '=', 's.issue_id')
                 ->leftJoin('types as t', 't.id', '=', 'p.type_id')
-                ->where('s.date', '=', $selectDate)
-                ->where('i.status', '=', 'publish')
-                ->where(function ($query) use ($teamID) {
-                    $query->where('p.team', '=', $teamID)
-                        ->orWhere('p.team', 'LIKE', $teamID . ',%')
-                        ->orWhere('p.team', 'LIKE', '%,' . $teamID . ',%')
-                        ->orWhere('p.team', 'LIKE', '%,' . $teamID);
+                ->where(function ($query) use ($selectDate, $teamID) {
+                    $query->where('s.date', '=', $selectDate)
+                        ->where('i.status', '=', 'publish')
+                        ->where(function ($query) use ($teamID) {
+                            $query->where('p.team', '=', $teamID)
+                                ->orWhere('p.team', 'LIKE', $teamID . ',%')
+                                ->orWhere('p.team', 'LIKE', '%,' . $teamID . ',%')
+                                ->orWhere('p.team', 'LIKE', '%,' . $teamID);
+                        });
+                })
+                ->orWhere(function ($query) use ($defaultProjects) {
+                    $query->whereIn('p.id', $defaultProjects);
                 })
                 ->orderBy('p_name', 'desc')
                 ->groupBy('i.id', 's.memo')
