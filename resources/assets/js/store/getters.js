9cfea8657fcbbb24bcb0a98e94632e21f292c0a8
KILALA_Kiet <quang.tran@kilala.vn>
2020-12-25 16:19:38 +0700

    update totaling page 20201225 31

--- resources/assets/js/store/getters.js
+++ resources/assets/js/store/getters.js
@@ -2,83 +2,93 @@ import moment from 'moment'
 import { vi, ja } from 'vuejs-datepicker/dist/locale'
 
 export default {
-    translateTexts: state => state.translateTexts,
-    loginUser: state => state.loginUser,
-    currentLang: state => state.currentLang,
-    currentTeam: state => state.currentTeam,
-    currentTeamOption: state => state.currentTeamOption,
-    reportNotify: state => state.reportNotify,
+	translateTexts: state => state.translateTexts,
+	loginUser: state => state.loginUser,
+	currentLang: state => state.currentLang,
+	currentTeam: state => state.currentTeam,
+	currentTeamOption: state => state.currentTeamOption,
+	reportNotify: state => state.reportNotify,
 
-    getTranslate(state) {
-        return (string) => {
-            return state.translateTexts ? state.translateTexts.get(string) : ''
-        }
-    },
+	getTranslate(state) {
+		return (string) => {
+			return state.translateTexts ? state.translateTexts.get(string) : ''
+		}
+	},
 
-    getObjectByID() {
-        return (Arr, id) => {
-            const result = Arr.filter(item => item.id === id)
-            return result.length ? result[0] : {}
-        }
-    },
+	getObjectByID() {
+		return (Arr, id) => {
+			const result = Arr.filter(item => item.id === id)
+			return result.length ? result[0] : {}
+		}
+	},
 
-    getArrObjectByID() {
-        return (Arr, id) => {
-            const result = Arr.filter(item => item.id === id)
-            return result.length ? result : []
-        }
-    },
+	getArrObjectByID() {
+		return (Arr, id) => {
+			const result = Arr.filter(item => item.id === id)
+			return result.length ? result : []
+		}
+	},
 
-    getLogTime() {
-        return (Arr, id, date) => {
-            const result = Arr.filter(item => item.id === id && item.date === date)
-            return result.length ? result : []
-        }
-    },
+	getLogTime() {
+		return (Arr, id, date) => {
+			const result = Arr.filter(item => item.id === id && item.date === date)
+			return result.length ? result : []
+		}
+	},
 
-    dateFormat() {
-        return (date, string = null, text='--') => {
-            if (moment(date).format() === 'Invalid date') return text
-            if (string) return moment(date).format(string)
-            return moment(date).format()
-        }
-    },
+	dateFormat() {
+		return (date, string = null, text = '--') => {
+			if (moment(date).format() === 'Invalid date') return text
+			if (string) return moment(date).format(string)
+			return moment(date).format()
+		}
+	},
 
-    customFormatter() {
-        return (date) => {
-            return moment(date).format('YYYY/MM/DD');
-        }
-    },
+	customFormatter() {
+		return (date) => {
+			return moment(date).format('YYYY/MM/DD');
+		}
+	},
 
-    getLangCode() {
-        const dataLang = {
-            vi: vi,
-            ja: ja
-        }
+	getLangCode() {
+		const dataLang = {
+			vi: vi,
+			ja: ja
+		}
 
-        return (data) => {
-            return dataLang[data.current]
-        }
-    },
+		return (data) => {
+			return dataLang[data.current]
+		}
+	},
 
-    setBackground() {
-        return (color) => {
-            return {
-                backgroundColor: color,
-                borderColor: color
-            }
-        }
-    },
+	setBackground() {
+		return (color) => {
+			return {
+				backgroundColor: color,
+				borderColor: color
+			}
+		}
+	},
 
+	disabledStartDates() {
+		return (date) => {
+			return { to: new Date(date), from: new Date() };
+		}
+	},
 
+	disabledEndDates() {
+		return (date) => {
+			return { from: new Date(date) };
+		}
+	},
 
-    getTeamText(state, getters) {
-        return (team) => {
-            if (typeof team === 'string' || team instanceof String) {
-                return team.split(',').map((item, index) => {
-                    return '<span>' + getters['getObjectByID'](state.currentTeamOption, +item).text + '</span>'
-                }).toString()
-            }
-        }
-    }
+	getTeamText(state, getters) {
+		return (team) => {
+			if (typeof team === 'string' || team instanceof String) {
+				return team.split(',').map((item, index) => {
+					return '<span>' + getters['getObjectByID'](state.currentTeamOption, +item).text + '</span>'
+				}).toString()
+			}
+		}
+	}
 }
