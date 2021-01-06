c2c200d4173e705ea9f51822d196184c8ac2e270
KILALA_Kiet <quang.tran@kilala.vn>
2021-01-06 14:22:39 +0700

    update page overview  20210106 #3

---
+++ database/migrations/2021_01_06_113736_create_total_page_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTotalPageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('total_page', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('type_id');
+            $table->integer('team_id');
+            $table->integer('page');
+            $table->string('date',6);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('total_page');
+    }
+}
